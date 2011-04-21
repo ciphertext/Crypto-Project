45fffc09ab8c04140fbcc5ea8502e242fb2f5726
Justin Sahs <justin.sahs@gmail.com>
2011-04-21 17:13:22 -0500

    Make Phase1Test more verbose

--- test/Phase1Test.cpp
+++ test/Phase1Test.cpp
@@ -7,20 +7,26 @@ using namespace Encryption::Keys;
 
 int main()
 {
-	for(int i = 0; i < 100; i++) {
-		KeyPair kp;
-		PublicKey pk = kp.getPublicKey();
-		PrivateKey sk = kp.getPrivateKey();
-		
-		Cipherbit c_one_bit = Encryptor::encrypt(1,pk);
-		Cipherbit c_zero_bit = Encryptor::encrypt(0,pk);
-		
-		bool one_bit = Encryptor::decrypt(c_one_bit,sk);
-		bool zero_bit = Encryptor::decrypt(c_zero_bit,sk);
+		cout << "Starting test." << endl;
+        for(int i = 0; i < 100; i++) {
+                cout << "Generating key pair " << i << endl;
+                KeyPair kp;
+                PublicKey pk = kp.getPublicKey();
+                PrivateKey sk = kp.getPrivateKey();
 
-		cout << "Trial " << i << endl;
-		cout << "One bit: " << (one_bit?"PASSED\n":"FAILED\n");
-		cout << "Zero bit: " << (!zero_bit?"PASSED\n":"FAILED\n");
-	}
-	return 0;
+                cout << "Encrypting one bit." << endl;
+                Cipherbit c_one_bit = Encryptor::encrypt(1,pk);
+                cout << "Encrypting zero bit." << endl;
+                Cipherbit c_zero_bit = Encryptor::encrypt(0,pk);
+
+                cout << "Decrypting one bit." << endl;
+                bool one_bit = Encryptor::decrypt(c_one_bit,sk);
+                cout << "Decrypting zero bit." << endl;
+                bool zero_bit = Encryptor::decrypt(c_zero_bit,sk);
+
+                cout << "One bit: " << (one_bit?"PASSED\n":"FAILED\n");
+                cout << "Zero bit: " << (!zero_bit?"PASSED\n":"FAILED\n");
+        }
+        return 0;
+	
 }
