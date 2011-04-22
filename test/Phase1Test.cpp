038a505b342e9945afa7d69763c7690f1bd3ee20
Justin Sahs <justin.sahs@gmail.com>
2011-04-21 21:56:58 -0500

    Phase1Test increased verbosity

--- test/Phase1Test.cpp
+++ test/Phase1Test.cpp
@@ -7,26 +7,39 @@ using namespace Encryption::Keys;
 
 int main()
 {
-		cout << "Starting test." << endl;
-        for(int i = 0; i < 100; i++) {
-                cout << "Generating key pair " << i << endl;
-                KeyPair kp;
-                PublicKey pk = kp.getPublicKey();
-                PrivateKey sk = kp.getPrivateKey();
+	cout << "Starting test." << endl;
+	for(int i = 0; i < 100; i++) {
+		cout << "Generating key pair " << i << endl;
+		KeyPair kp;
+		PublicKey pk = kp.getPublicKey();
+		PrivateKey sk = kp.getPrivateKey();
 
-                cout << "Encrypting one bit." << endl;
-                Cipherbit c_one_bit = Encryptor::encrypt(1,pk);
-                cout << "Encrypting zero bit." << endl;
-                Cipherbit c_zero_bit = Encryptor::encrypt(0,pk);
+		cout << "pk: x(";
+		for(unsigned int i = 0; i < pk.xsize(); i++)
+			cout << pk.getX(i) << ", ";
+		cout << "), y(";
+		for(unsigned int i = 0; i < pk.ysize(); i++)
+			cout << pk.getY(i) << ", ";
+		cout << ")" << endl;
 
-                cout << "Decrypting one bit." << endl;
-                bool one_bit = Encryptor::decrypt(c_one_bit,sk);
-                cout << "Decrypting zero bit." << endl;
-                bool zero_bit = Encryptor::decrypt(c_zero_bit,sk);
+		cout << "sk: s(";
+		for(unsigned int i = 0; i < sk.size(); i++)
+			cout << sk.getBit(i) << ", ";
+		cout << ")" << endl;
 
-                cout << "One bit: " << one_bit << endl;
-                cout << "Zero bit: " << zero_bit << endl;
-        }
-        return 0;
+		cout << "Encrypting one bit." << endl;
+		Cipherbit c_one_bit = Encryptor::encrypt(1,pk);
+		cout << "Encrypting zero bit." << endl;
+		Cipherbit c_zero_bit = Encryptor::encrypt(0,pk);
+
+		cout << "Decrypting one bit." << endl;
+		bool one_bit = Encryptor::decrypt(c_one_bit,sk);
+		cout << "Decrypting zero bit." << endl;
+		bool zero_bit = Encryptor::decrypt(c_zero_bit,sk);
+
+		cout << "One bit: " << one_bit << endl;
+		cout << "Zero bit: " << zero_bit << endl;
+	}
+	return 0;
 	
 }
