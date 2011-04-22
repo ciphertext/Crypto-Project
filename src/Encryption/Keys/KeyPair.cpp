0bede5ebad6c230ad9169d2a229927250a9a6445
Tucker Smith <tss063000@utdallas.edu>
2011-04-22 14:51:52 -0500

    Refactored keypair.cpp
    Magically it works now.

--- src/Encryption/Keys/KeyPair.cpp
+++ src/Encryption/Keys/KeyPair.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 using namespace Encryption;
 using namespace Encryption::Keys;
+using namespace boost;
 
 // Lambda - security parameter
 const long int _lambda = 4;
@@ -26,43 +27,83 @@ const long int _theta = 4;
 const long int _bigTheta = 60;
 
 KeyPair::KeyPair()
+: rd(),
+  base_gen(rd())
 {
 	// sk* = p
 	// p = random odd number [2^_eta-1, 2^_eta)
 	// generate random number between 2^_eta-1 / 2, 2^_eta / 2
 	// multiply by 2, subtract 1 to ensure odd number
-	boost::random_device rd;
-	boost::mt19937 base_gen(rd()); // seed based on random output from /dev/urandom
-	boost::variate_generator<boost::mt19937&, boost::uniform_int<> >
-			generate_p(base_gen,
-					   boost::uniform_int<>((int) pow(2.0, (double) _eta-1)/2, (int) pow(2.0, (double) _eta)/2 - 1));
+	var_gen_t generate_p(base_gen,
+					   uniform_int<>((int) pow(2.0, (double) _eta-1)/2, (int) pow(2.0, (double) _eta)/2 - 1));
 
-	long int p = (2 * generate_p()) - 1;
+	int_t p = (2 * generate_p()) - 1;
 	
-	// pk*
+	pk_t pk = getPk(p);	
+	
+	s_t S = getS();
+	
+	bitmap_t sArrow= getSArrow(S);
+	
+	theta_t u = getBigTheta();
+	
+	theta_t u2= getTheta();
+	
+	int_t u_final = getUFinal(p,u2);
+
+	doSomethingWithU(S,u,u2,u_final);
+	
+	y_t y = getY(u);
+
+	// private key is sArrow
+	privateKey = PrivateKey(sArrow);
+	// public key is pk, y, and encrypted private key	
+	
+   sk_t sk = getSk(sArrow,pk,y);
+
+	publicKey = PublicKey(pk, y, sk);
+}
+
+
+
+
+PublicKey KeyPair::getPublicKey()
+{
+	return publicKey;
+}
+
+PrivateKey KeyPair::getPrivateKey()
+{	
+	return privateKey;
+}
+
+
+
+
+
+KeyPair::pk_t KeyPair::getPk(int_t p)
+{
+  
+  	// pk*
 	// for i = 0 to _tau
 	// choose random q, [0, 2^_gamma / p)
 	// choose random r, (-2^_rho, 2^_rho)
 	// x_i = pq+r
 	// x_0 largest and restart unless x_0 is odd and x_0 mod p is even
-	boost::variate_generator<boost::mt19937&, boost::uniform_int<> >
-			generate_q(base_gen,
-					   boost::uniform_int<>(0, (long int) pow(2.0,(double) _gamma)/p - 1));
+	var_gen_t generate_q(base_gen,
+					   uniform_int<>(0, (long int) pow(2.0,(double) _gamma)/p - 1));
 
-	boost::variate_generator<boost::mt19937&, boost::uniform_int<> >
-			generate_r(base_gen,
-					   boost::uniform_int<>(-(int) pow(2.0, (double) _rho) + 1, (int) pow(2.0, (double) _rho) - 1));
-
-	bool restart = true;
-	vector<long int> pk;
-	vector<long int>::iterator it;
+	var_gen_t generate_r(base_gen,
+					   uniform_int<>(-(int) pow(2.0, (double) _rho) + 1, (int) pow(2.0, (double) _rho) - 1));
+  
+  	pk_t pk;
 	
-	while(restart) {	
+	while(true) {	
 		int largestIndex = 0;
 		for (int i = 0; i <= _tau; i++) {		
-			long int q = generate_q();
-			long int r = generate_r();
-			long int x = (p * q) + r;
+			int_t q = generate_q();
+			int_t r = generate_r();
+			int_t x = (p * q) + r;
 			
 			pk.push_back(x);
 			
@@ -74,66 +115,100 @@ KeyPair::KeyPair()
 		// store temp value
 		// delete from location
 		// insert to front of vector
-		long int temp = pk[largestIndex];
-		pk.erase(pk.begin() + largestIndex - 1);
-		it = pk.begin();
-		pk.insert(it , temp);
+		int_t temp = pk[largestIndex];
+		pk[largestIndex]=pk[0];
+		pk[0]=temp;
+	
 		
 		// check if x_0 is odd and x_0 mod p is even
 		// if not, restart
 		if((temp % 2 == 1) && ((temp % p)%2 == 0))
-			restart = false;
+			break;
 		else
 			pk.clear();
 	}
 	
-	// x_p = round(2^k/p)
-	long int xP = (long int) round(pow(2.0, (double) _kappa) / p);
-	
-	// sArrow = random big-_theta bit bector with hamming weight _theta
-	boost::variate_generator<boost::mt19937&, boost::uniform_int<> >
-			generate_s(base_gen,
-						boost::uniform_int<>(0,_bigTheta-1));
+	return pk;
+}
+
+
+KeyPair::s_t KeyPair::getS()
+{
+ 	// sArrow = random big-_theta bit bector with hamming weight _theta
+	var_gen_t generate_s(base_gen, uniform_int<>(0,_bigTheta-1));
 	
 	// choose random S
-	set<int> S; //Use set to guarantee unique elements
-	while(S.size() < (unsigned long int) _theta) {
+	s_t S; //Use set to guarantee unique elements
+	while(S.size() < (uint_t) _theta) {
 		S.insert(generate_s());
-	}
+	} 
 	
+	return S;
+}
+
+
+KeyPair::bitmap_t KeyPair::getSArrow(s_t S)
+{
 	// create sArrow
 	// set all values to 0
 	// set indices i in S to 1
 	vector<bool> sArrow(_bigTheta, false);
-	for(set<int>::iterator it = S.begin(); it != S.end(); it++)
+	for(s_t::iterator it = S.begin(); it != S.end(); it++)
 		sArrow[*it] = true;
-	
-	// generate u_i = [0, 2^k+1) for i = 1...big-_theta
+	return sArrow;
+}
+
+
+KeyPair::theta_t KeyPair::getBigTheta()
+{
+		// generate u_i = [0, 2^k+1) for i = 1...big-_theta
 	// sum of u_i, where i in S, = x_p mod 2^k+1
-	vector<long int> u;
-	boost::variate_generator<boost::mt19937&, boost::uniform_int<long int> >
-			generate_u(base_gen,
-					   boost::uniform_int<long int>(0, (long int) pow(2.0, (double) _kappa+1) -1));
-										
+	theta_t u;
+	var_gen_theta_t generate_u(base_gen,
+					   uniform_int<long int>(0, (long int) pow(2.0, (double) _kappa+1) -1));
+	
 	/* generate _bigTheta - _theta random integers */
 	for(int i = 0; i < _bigTheta - _theta; i++)
 		u.push_back(generate_u());
 	
+	return u;
+}
+
+
+KeyPair::theta_t KeyPair::getTheta()
+{
+		// generate u_i = [0, 2^k+1) for i = 1...big-_theta
+	// sum of u_i, where i in S, = x_p mod 2^k+1
+	theta_t u2;
+	var_gen_theta_t generate_u(base_gen,
+					   uniform_int<long int>(0, (long int) pow(2.0, (double) _kappa+1) -1));
+	
 	/* generate _theta - 1 more random integers */
-	vector<long int> u2;
 	for(int i = 0; i < _theta - 1; i++)
 		u2.push_back(generate_u());
+	
+	return u2;
+}
 
-	/* calculate a final integer such that the sum
+KeyPair::int_t KeyPair::getUFinal(int_t p, theta_t u2)
+{
+	int_t xP = (int_t) round(pow(2.0, (double) _kappa) / p);
+	
+		/* calculate a final integer such that the sum
 	 * of the u2 integers = xP mod 2^k+1 */
-	long int sum = 0;
-	for(vector<long int>::iterator it = u2.begin(); it != u2.end(); it++) {
-		sum = sum + *it;
-		sum = sum % (long int) pow(2.0, (double) _kappa +1);
+	int_t sum = 0;
+	BOOST_FOREACH(int_t i , u2)
+	{
+		sum += i;
+		sum %= (int_t) pow(2.0, (double) _kappa +1);
 	}
-	long int u_final = (xP - sum) % (long int) pow(2.0, (double) _kappa +1);
+	
+	return (xP - sum) % (int_t) pow(2.0, (double) _kappa +1);
+}
 
-	for(set<int>::iterator it = S.begin(); it != S.end(); it++) {
+void KeyPair::doSomethingWithU(s_t S, theta_t & u, theta_t u2, int_t u_final)
+{
+	for(s_t::iterator it = S.begin(); it != S.end(); it++) {
 		vector<long int>::iterator ind = u.begin() + *it;
 		if(!u2.empty()) {
 			u.insert(ind, u2.back());
@@ -142,32 +217,25 @@ KeyPair::KeyPair()
 		else 
 			u.insert(ind, u_final);
 	}
-	
-	// calculate y_i = u_i/2^k
-	vector<boost::rational<long int> > y;
-	for(int i = 0; i < _bigTheta; i++)
-		y.push_back(boost::rational<long int>(u[i], (long int) pow(2.0, (double) _kappa)));
-	
-	// private key is sArrow
-	this->privateKey = PrivateKey(sArrow);
-	
-	// public key is pk, y, and encrypted private key	
-	vector<Cipherbit> sk;
-	this->publicKey = PublicKey(pk, y, sk);
-
-	for(unsigned int z = 0; z < sArrow.size(); z++)
-		sk.push_back(Encryptor::encrypt(sArrow[z], this->publicKey));
-
-	this->publicKey = PublicKey(pk, y, sk);
 }
 
 
-PublicKey KeyPair::getPublicKey()
+
+KeyPair::y_t KeyPair::getY(theta_t u)
 {
-	return this->publicKey;
-}
+	// calculate y_i = u_i/2^k
+	y_t y;
+	for(int i = 0; i < _bigTheta; i++)
+		y.push_back(rational<long int>(u[i], (long int) pow(2.0, (double) _kappa)));
+	
+	return y;
+}	
 
-PrivateKey KeyPair::getPrivateKey()
-{	
-	return this->privateKey;
+KeyPair::sk_t  KeyPair::getSk(bitmap_t sArrow, pk_t pk, y_t y)
+{
+	sk_t sk;
+	
+	for(unsigned int z = 0; z < sArrow.size(); z++)
+		sk.push_back(Encryptor::encrypt(sArrow[z], PublicKey(pk, y, sk)));
+	return sk;
 }
--- src/Encryption/Keys/KeyPair.hpp
+++ src/Encryption/Keys/KeyPair.hpp
@@ -11,7 +11,8 @@
 #include "Encryption/Keys/PublicKey.hpp"
 #include "Encryption/Keys/PrivateKey.hpp"
 #include "Encryption/Encryptor.hpp"
-
+#include <algorithm>
+#include <boost/foreach.hpp>
 
 
 namespace Encryption
@@ -20,14 +21,47 @@ namespace Encryption
 	{
 		class KeyPair
 		{
-			private:
-				PrivateKey privateKey;
-				PublicKey publicKey;
-
+			
 			public:
 				KeyPair();
 				PublicKey getPublicKey();
 				PrivateKey getPrivateKey();
+				
+			private:
+				PrivateKey privateKey;
+				PublicKey publicKey;
+				
+				boost::random_device rd;
+				boost::mt19937 base_gen;
+   			
+				typedef	boost::variate_generator<boost::mt19937&, boost::uniform_int<> > var_gen_t;
+				typedef  boost::variate_generator<boost::mt19937&, boost::uniform_int<long int> > var_gen_theta_t;
+				typedef long int int_t;
+				typedef unsigned long int uint_t;
+				typedef std::vector<bool> bitmap_t;
+				typedef std::vector<int_t> pk_t;
+				typedef std::vector<int_t> theta_t;
+				typedef std::set<int> s_t; 
+				typedef std::vector<boost::rational<int_t> > y_t;
+				typedef std::vector<Cipherbit> sk_t;
+				
+				pk_t getPk(int_t p);
+				s_t getS();
+				bitmap_t getSArrow(s_t S);
+				theta_t getBigTheta();
+				theta_t getTheta();
+				int_t getUFinal(int_t p, theta_t u2);
+				void doSomethingWithU(s_t S, theta_t & u, theta_t u2, int_t u_final);
+				y_t getY(theta_t u);
+				sk_t getSk(bitmap_t sArrow, pk_t pk, y_t y);
+				
+
+
+
+
+
+
+
 		};
 	}
 }
