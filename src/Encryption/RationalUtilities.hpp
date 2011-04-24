2350091559dbf9c013bbb65c04ca309e691ff0fb
Justin Sahs <justin.sahs@gmail.com>
2011-04-24 18:57:53 -0500

    Canonicalize in rational utilities

--- src/Encryption/RationalUtilities.hpp
+++ src/Encryption/RationalUtilities.hpp
@@ -3,23 +3,30 @@
 
 #include <gmpxx.h>
 
-
 inline mpq_class r_floor(mpq_class n) {
-	return mpq_class(n.get_num() - (n.get_num() % n.get_den()), n.get_den());
+    mpq_class ret(n.get_num() - (n.get_num() % n.get_den()), n.get_den());
+    ret.canonicalize();
+    return ret;
 }
 
 inline mpq_class r_round(mpq_class n) {
-	mpq_class half(1,2);
-	return r_floor(n + half);
+    mpq_class half(1,2);
+    mpq_class ret = r_floor(n + half);
+    ret.canonicalize();
+    return ret;
 }
 
 inline mpq_class r_modulo(mpq_class a, int b) {
-	return mpq_class(a.get_num() % (b*a.get_den()),a.get_den());
+    mpq_class ret(a.get_num() % (b*a.get_den()),a.get_den());
+    ret.canonicalize();
+    return ret;
 }
 
 inline mpq_class fix_precision_bits(mpq_class a, int bits) {
-	mpz_class power = mpz_class(2) << (bits-1);
-	return r_floor(a * power) / power;
+    mpz_class power = mpz_class(2) << (bits-1);
+    mpq_class ret = r_floor(a * power) / power;
+    ret.canonicalize();
+    return ret;
 }
 
 #endif
